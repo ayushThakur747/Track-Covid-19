05b2c8c6f759744f574498d60c8d290cb60f6f9a
ayushthakur747 <itsathakur30@gmail.com>
2020-07-26 21:26:54 +0530

    Updates

---
+++ precache-manifest.383e6ffa8a972cb158c7c8a5cd08d03c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb8a8d62e18f5b14e8c3a3b68213092",
+    "url": "/Track-Covid-19/index.html"
+  },
+  {
+    "revision": "02a2e8830e291b1fc640",
+    "url": "/Track-Covid-19/static/css/main.401d8559.chunk.css"
+  },
+  {
+    "revision": "95d31d9a3229f29fe95a",
+    "url": "/Track-Covid-19/static/js/2.aaea388d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Track-Covid-19/static/js/2.aaea388d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a2e8830e291b1fc640",
+    "url": "/Track-Covid-19/static/js/main.cb13f8fc.chunk.js"
+  },
+  {
+    "revision": "13fc98b6626f070b09d4",
+    "url": "/Track-Covid-19/static/js/runtime-main.c233a7f1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Track-Covid-19/static/media/covidImg.d7265326.png"
+  }
+]);
